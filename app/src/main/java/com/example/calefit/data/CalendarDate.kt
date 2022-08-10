1cb5fcda42e053b79e0b9d5d00b0ad8cbd62ddbd
junseokseo <junseoks9306@gmail.com>
2022-08-10 18:34:29 +0900

    fix: payload bundle type is changed

--- app/src/main/java/com/example/calefit/data/CalendarDate.kt
+++ app/src/main/java/com/example/calefit/data/CalendarDate.kt
@@ -1,15 +1,18 @@
 package com.example.calefit.data
 
+import java.io.Serializable
+
 sealed class CalendarDate {
 
     data class ItemDays(
         val id: Int,
-        val date: String,
-        val onSchedule: Boolean = false,
+        val day: String,
+        val date: String = "",
+        val hasSchedule: Boolean = false,
         val isClicked: Boolean = false,
         val isVisible: Boolean = false,
         val isToday: Boolean = false,
-    ) : CalendarDate()
+    ) : CalendarDate(), Serializable
 
     data class ItemHeader(
         val dateIndicator: String,
