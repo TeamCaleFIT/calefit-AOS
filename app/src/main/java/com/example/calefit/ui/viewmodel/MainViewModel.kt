440446f44a415254d07adf682ec2a820dcb6c75a
junseokseo <junseoks9306@gmail.com>
2022-08-17 13:55:36 +0900

    feat: add domain layer

--- app/src/main/java/com/example/calefit/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/calefit/ui/viewmodel/MainViewModel.kt
@@ -1,72 +1,51 @@
 package com.example.calefit.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.calefit.data.Aggregate
 import com.example.calefit.data.ExerciseList
+import com.example.calefit.usecase.GetExerciseListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(
+    getExerciseListUseCase: GetExerciseListUseCase
+) : ViewModel() {
 
     private val _clickedDate = MutableStateFlow("")
+    val clickedDate = _clickedDate.asStateFlow()
 
     private val _exerciseList = MutableStateFlow(
-        listOf(
-            ExerciseList(
-                date = "2022-08-09",
-                list = listOf(
-                    ExerciseList.Exercise(
-                        id = "1",
-                        name = "스쿼트",
-                        cycle = "3회",
-                        weight = "100kg"
-                    ),
-                    ExerciseList.Exercise(
-                        id = "2",
-                        name = "덤벨",
-                        cycle = "10회",
-                        weight = "40kg"
-                    ),
-                    ExerciseList.Exercise(
-                        id = "3",
-                        name = "벤치프레스",
-                        cycle = "5회",
-                        weight = "120kg"
-                    )
-                )
-            ),
-            ExerciseList(
-                date = "2022-08-10",
-                list = listOf(
-                    ExerciseList.Exercise(
-                        id = "1",
-                        name = "런지",
-                        cycle = "5회",
-                        weight = "20kg"
-                    )
-                )
-            )
-        )
+        getExerciseListUseCase()
     )
 
-    val exerciseList = MutableStateFlow<List<ExerciseList.Exercise>>(listOf())
+    val exerciseMap = _exerciseList.map {
+        when (it) {
+            is Aggregate.Success -> {
+                dataLoading.value = false
+                it.data
+            }
+            is Aggregate.Error -> {
+                dataLoading.value = false
+                emptyMap()
+            }
+            is Aggregate.Loading -> {
+                dataLoading.value = true
+                emptyMap()
+            }
+        }
+    }
+
+    val dataLoading = MutableStateFlow(false)
 
     fun setDate(date: String) {
         _clickedDate.value = date
         require(date.isNotEmpty())
-        val list = _exerciseList.value.find { it.date == date }
-        if (list != null) {
-            exerciseList.value = list.list
-        } else {
-            exerciseList.value = listOf(
-                ExerciseList.Exercise(
-                    id = "1",
-                    name = "",
-                    cycle = "",
-                    weight = "",
-                )
-            )
-        }
     }
 }
