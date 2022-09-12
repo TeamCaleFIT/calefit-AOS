ca92336c3f3b100c7ba1850bf61b1da138a2d6ef
junseokseo <junseoks9306@gmail.com>
2022-09-12 15:21:39 +0900

    feat: insert a edittext soft keyboard hiding method in home activity

--- app/src/main/java/com/example/calefit/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/calefit/ui/home/HomeActivity.kt
@@ -1,6 +1,11 @@
 package com.example.calefit.ui.home
 
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
@@ -45,5 +50,21 @@ class HomeActivity : AppCompatActivity() {
 
     private fun hideBottomNav() {
         binding.navBottom.isVisible = false
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
+        if (event?.action == MotionEvent.ACTION_DOWN) {
+            val view = currentFocus
+            if (view is EditText) {
+                val outRect = Rect()
+                view.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    view.clearFocus()
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(view.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 }
