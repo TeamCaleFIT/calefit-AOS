8c447a431cf7aec92210019166847aa3bba2d884
junseokseo <junseoks9306@gmail.com>
2022-09-25 17:14:35 +0900

    feat: make exerciseDetailFragment
    - make exerciseDetailFragment to ready for displaying the data
    - override methods from baseViewModel

--- app/src/main/java/com/example/calefit/ui/home/detali/ExerciseDetailViewModel.kt
+++ app/src/main/java/com/example/calefit/ui/home/detali/ExerciseDetailViewModel.kt
@@ -1,15 +1,45 @@
 package com.example.calefit.ui.home.detali
 
+import android.util.Log
+import com.example.calefit.data.Aggregate
+import com.example.calefit.data.ExerciseList
 import com.example.calefit.data.ExerciseSelection
 import com.example.calefit.ui.common.NestedRecyclerBaseViewModel
-import com.example.calefit.ui.common.InputCategory
+import com.example.calefit.data.UserRecyclerviewClick
+import com.example.calefit.usecase.GetSpecificDateExerciseListOrEmptyListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class ExerciseDetailViewModel @Inject constructor() : NestedRecyclerBaseViewModel() {
-    override fun addAdditionalExercise(exerciseList: List<ExerciseSelection>) {
-        TODO("Not yet implemented")
+class ExerciseDetailViewModel @Inject constructor(
+    private val getSpecificDateExerciseListOrEmptyListUseCase: GetSpecificDateExerciseListOrEmptyListUseCase
+) : NestedRecyclerBaseViewModel() {
+
+    private val _exerciseList = MutableStateFlow(ExerciseList(list = emptyList()))
+    val exerciseList = _exerciseList.asStateFlow()
+
+    val dataLoading = MutableStateFlow(false)
+
+    override fun addAdditionalExercise(selectedExerciseList: List<ExerciseSelection>) {
+        if (selectedExerciseList.isEmpty()) {
+            return
+        }
+
+        _exerciseList.update { currentList ->
+            val newList = currentList.list.toMutableList()
+            selectedExerciseList.forEach { selectedExercise ->
+                newList.add(
+                    ExerciseList.Exercise(
+                        id = (newList.size + 1).toString(),
+                        name = selectedExercise.name,
+                    )
+                )
+            }
+            currentList.copy(list = newList)
+        }
     }
 
     override fun addAdditionalCycle(position: Int): Boolean {
@@ -24,14 +54,26 @@ class ExerciseDetailViewModel @Inject constructor() : NestedRecyclerBaseViewMode
         TODO("Not yet implemented")
     }
 
-    override fun getUserInputValue(
-        outerPosition: Int,
-        innerPosition: Int,
-        value: String?,
-        category: InputCategory
-    ) {
+    override fun setCurrentAdapterPositions(userRecyclerviewClick: UserRecyclerviewClick) {
         TODO("Not yet implemented")
     }
 
+    override fun setUserSelectedNumber(number: Int) {
+        TODO("Not yet implemented")
+    }
 
+    fun setExerciseByDate(date: String) {
+        when (val data = getSpecificDateExerciseListOrEmptyListUseCase(date)) {
+            is Aggregate.Success -> {
+                dataLoading.value = false
+                _exerciseList.value = data.data
+            }
+            is Aggregate.Error -> {
+                dataLoading.value = false
+            }
+            is Aggregate.Loading -> {
+                dataLoading.value = true
+            }
+        }
+    }
 }
