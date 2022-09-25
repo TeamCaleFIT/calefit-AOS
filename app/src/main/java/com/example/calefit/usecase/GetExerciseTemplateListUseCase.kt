bf0d0cf15bcf1fa7bb6ea20ec105eed272198b2a
junseokseo <junseoks9306@gmail.com>
2022-09-25 16:47:37 +0900

    feat: Add TemplateListUseCase
    - get data from repository and returns template exerciseLists

---
+++ app/src/main/java/com/example/calefit/usecase/GetExerciseTemplateListUseCase.kt
@@ -0,0 +1,41 @@
+package com.example.calefit.usecase
+
+import com.example.calefit.data.Aggregate
+import com.example.calefit.data.ExerciseList
+import com.example.calefit.data.ExerciseTemplateSummary
+import com.example.calefit.repository.ExerciseRepository
+import java.util.*
+import javax.inject.Inject
+
+class GetExerciseTemplateListUseCase @Inject constructor(
+    private val repository: ExerciseRepository
+) {
+    operator fun invoke(): Aggregate<List<ExerciseTemplateSummary>> {
+        return when (val rawData = repository.getExerciseDataFromRepository()) {
+            is Aggregate.Success -> {
+                val templateList = mutableListOf<ExerciseTemplateSummary>()
+                rawData.data.forEach { map ->
+                    val exerciseList = map.value
+                    if (exerciseList.templateName != DEFAULT_TEMPLATE_NAME) {
+                        templateList.add(
+                            ExerciseTemplateSummary(
+                                id = UUID.randomUUID(),
+                                templateTitle = exerciseList.templateName,
+                                exerciseCount = exerciseList.list.size,
+                                exerciseDate = exerciseList.date
+                            )
+                        )
+                    }
+                }
+                Aggregate.Success(templateList)
+            }
+            else -> {
+                Aggregate.Error(IllegalArgumentException())
+            }
+        }
+    }
+
+    private companion object {
+        const val DEFAULT_TEMPLATE_NAME = ""
+    }
+}
